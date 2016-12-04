9714c07cde7b045140041e0fc13c44a637b47cc7
Ian Li <ianli6362@gmail.com>
2016-12-04 15:15:34 -0800

    Initial Commit

---
+++ ezMoney/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ezMoney
+//
+//  Created by Ian Li on 12/4/16.
+//  Copyright © 2016 Ian Li. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
